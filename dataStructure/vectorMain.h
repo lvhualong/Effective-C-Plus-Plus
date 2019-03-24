c4d5cbee533ef019aac3acec629d4bc87a9224af
sunshine <hualong1995@hotmail.com>
2019-03-24 09:07:36 +0800

    update vector

--- dataStructure/vectorMain.h
+++ dataStructure/vectorMain.h
@@ -3,9 +3,22 @@
 #define MYVECTOR_H
 
 
+typedef int Rank; // vector 秩
 
 
+template <typename T> class Vector 
+{
+protected:
+    Rank _size;   //规模
+    int _capacity; //容量
+    T* _elem;    //数据区
 
+
+public: 
+    //构造函数
+    Vector()
+    {}    
+}
 
 
 
