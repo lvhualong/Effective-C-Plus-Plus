92271b6894613662a0e0308e8bc545b8806e357d
sunshine <hualong1995@hotmail.com>
2019-04-23 15:36:50 +0800

    update programming

--- programming/nuimax2019/01_test.cpp
+++ programming/nuimax2019/01_test.cpp
@@ -2,31 +2,30 @@
 /* *********************************************************
  * 问题描述：
  *
- * 
+ *
  * *********************************************************
  */
 
 #include <iostream>
 #include <vector>
-#include <list>   //容易删改 方便前后删除
-#include <deque>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-
-class Solution{
-public:
-    void function(int num){
-
-    }
-};
-
 int main() {
-    Solution solution;
+    vector<string> strs;
+    int nums;
 
-    std::cout << " " << std::endl;
-
+    cin >> nums;
+    for(int i=0; i<nums; i++)
+    {
+        string temp; cin >> temp;
+        strs.push_back(temp);
+    }
+    sort(strs.begin(), strs.end());
+    for(auto str:strs)
+        cout << str << endl;
     return 0;
-
 }
 /* *********************************************************
  * 解题思路：
