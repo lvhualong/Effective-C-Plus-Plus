43a77c954b112233b369b979c68110054f0332c7
sunshine <hualong1995@hotmail.com>
2019-05-08 21:02:36 +0800

    update programming

--- today/01_test.cpp
+++ today/01_test.cpp
@@ -1,64 +1,44 @@
 
-/* *********************************************************
- * 问题描述：类似于数组的最大连续子数组和问题， 这是一个最大容器(矩形面积)问题
- *
- * 给定n个非负整数，a0, a1……，每个整数代表一个坐标(i, ai)
- * 根据这些坐标可以画出n个垂直于x轴的垂直线，每条线的端点(i,0)与(i, ai)
- * 在这些相中，找到两条垂线，和x轴一起组成可以盛水的容器，容器不可倾斜
- * 要求容器盛水最多，并输入最大值
- *
- * 注意：n最小为2， 要求写出算法为线性时间复杂度O(n)
- *
- *
- * 输入描述：
- * 输入多行，给出n个非负整数ai,
- * 数字用逗号分开，且逗号跟数字之间有任意空白字符
- * 1,
- * 8,  2,
- * 6, 5
- * 输出最大容量
- * 15
- *
- * 解释：
- * 5*(4-1)
- * *********************************************************
- */
-
 #include <iostream>
 #include <vector>
-#include <string>
-#include <cstdio>
-#include <cstring>
+
 using namespace std;
-class Solution {
-public:
 
-    // 字符串切法，转换成整数
-    void strToNum(string str, vector<int>& nums)
-    {
-        char* c_str = new char [str.length()+1];
-        strcpy(c_str, str.c_str());
-        char* c_value;
-        c_value = strtok (c_str," ,.-");
-        while(c_value != NULL)
-        {
-            int temp = (*c_value-'0');
-            nums.push_back(temp);
-            c_value = strtok (NULL,", ");
-        }
-        delete [] c_str;
+void gift(unsigned int gift_num, unsigned int child_k,int cur_gift, int cur_k, vector<int>& out, vector<vector<int>>& res){
+    if(cur_k==child_k){
+        int num = gift_num - cur_gift;
+        out.push_back(num);
+        res.push_back(out);
+        out.pop_back();
+        return;
     }
-};
 
-int main() {
+    for (int i = cur_gift; i <= gift_num; ++i) {
+        int num = i - cur_gift;
+        out.push_back(num);
+        gift(gift_num,child_k,i,cur_k+1,out,res);
+        out.pop_back();
+    }
+}
+
+int main(){
+    unsigned int n,k;
+    cin>>n>>k;
+    vector<vector<int>> res;
+    vector<int> out;
+    gift(n,k,0,1,out,res);
+
+    for (auto& subres:res){
+        for (int i = 0; i < subres.size(); ++i) {
+            for (int j = 0; j <subres[i] ; ++j) {
+                cout<<"*";
+            }
+            if(i!=subres.size()-1){
+                cout<<'|';
+            }
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
-/* *********************************************************
- * 解题思路：
- *
- * ----------------------------------
- * 测试示例：
- *
- * *********************************************************
- */
